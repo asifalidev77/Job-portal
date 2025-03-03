ed5082be55c5e345044368f81de48277200d978e
asifalidev77 <asifali010122@gmail.com>
2025-03-04 01:16:25 +0500

    responsive hero section

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -1,33 +1,38 @@
 "use Client";
-import { Box, Button, Container, TextField, Typography} from "@mui/material";
-import Grid from "@mui/material/Grid2";
+import { Box, Button, Container, TextField, Typography } from "@mui/material";
 import HeroImage from "../../assets/PNG/herosection.png";
 import Image from "next/image";
 export default function HeroSection() {
-    return (
-      <>
-      <Box
-      sx={{
-        backgroundColor: "#ffffff",
-        minHeight: "80vh",
-        display: "flex",
-        flexWrap: "nowrap",
-        alignItems: "center",
-        py: { xs: 4, md: 8 },}}>
-        <Container maxWidth="lg">
-            <Grid container spacing={4} alignItems="center">
-            <Grid item xs={6} >
-              <Typography variant="h3" sx={{ fontWeight: "bold"}}>
-              We Help You Find <br /> Your Dream Job
-                </Typography>
-                <Typography variant="body1" color="text.secondary" mb={3}>
-              Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-              Quis <br/> autem vel eum iure reprehenderit qui in ea voluptate.
+  return (
+    <>
+      <Container maxWidth="lg">
+        <Box
+          sx={{
+            backgroundColor: "#ffffff",
+            minHeight: "60vh",
+            display: "flex",
+            flexDirection: { xs: "column", md: "row" },
+            alignItems: "center",
+            width: "100%",
+            py: { xs: 4, md: 8 },
+            gap: { xs: 4, md: 0 },
+          }}
+        >
+          <Box sx={{ width: { xs: "100%", md: "45%" },
+            textAlign: "left",
+            px: { xs: 2, md: 0 },
+             }}>
+            <Typography variant="h3" sx={{ fontWeight: "bold", fontSize: { xs: "2rem", md: "3rem" }, }}>
+              We Help You Find Your Dream Job
+            </Typography>
+            <Typography variant="body1" color="text.secondary" sx={{ my: 3, fontSize: { xs: "14px", md: "16px" } }}>
+              Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Quis autem vel eum iure reprehenderit qui in ea voluptate.
             </Typography>
             <Box
               sx={{
                 display: "flex",
-                flexDirection: { xs: "column", sm: "row" },
+                flexDirection: { xs: "column", md: "row" },
                 gap: 2,
                 alignItems: "center",
                 backgroundColor: "#fff",
@@ -42,6 +47,7 @@ export default function HeroSection() {
                 label="Job Title or Company"
                 variant="outlined"
                 size="small"
+                sx={{ fontSize:"20px" }}
               />
               <TextField
                 fullWidth
@@ -49,26 +55,31 @@ export default function HeroSection() {
                 label="Select Country"
                 variant="outlined"
                 size="small"
+                
+              ></TextField>
+              <Button
+                variant="contained"
+                size="small"
+                sx={{
+                  py: "8px",
+                  width: { xs: "100%", },
+                }}
               >
-              </TextField>
-              <Button variant="contained" size="small" >
                 Search Job
               </Button>
             </Box>
-            </Grid>
-            <Grid item xs={6} sx={{ textAlign: "center", display: "flex", justifyContent: "end"}}>
-              <Image
-                src={HeroImage}
-                alt="Hero Image"
-                width={500}
-                height={500}
-              />
-            </Grid>
-            
-            </Grid>
-          </Container>
-      </Box>
-      </>
-    );
-  }
-  
+          </Box>
+          <Box sx={{ width: { xs: "100%", md: "50%" }, display: "flex", justifyContent: "end" }}>
+            <Image
+              src={HeroImage}
+              alt="Hero Image"
+              width={888}
+              height={847}
+              objectFit="contain"
+            />
+          </Box>
+        </Box>
+      </Container>
+    </>
+  );
+}
